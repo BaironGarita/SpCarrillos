6764b031b874e3d2a2035ed0014649cece2b1ff1
Bairon Garita Ballestero <ingbairongarita@gmail.com>
2025-04-28 17:06:41 -0600

    feat: refactor Tailwind CSS configuration and update HTML files to use external config

---
+++ JS/tailwind_config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#272C5F",
+        secondary: "#D8B315",
+        accent: "#3498db",
+      },
+    },
+  },
+};
